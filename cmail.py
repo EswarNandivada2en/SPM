a0929cc0d70fc60022ad7c5f3c6b3c18896ad33f
EswarNandivada2en <eswar@codegnan.com>
2023-04-03 12:07:27 +0530

    first commit

---
+++ cmail.py
@@ -0,0 +1,24 @@
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('eswar@codegnan.com','sppcchhmtygraxzh')
+    msg=EmailMessage()
+    msg['From']='eswar@codegnan.com.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
+
+    
+
+
+
+
+
+
+
+        
+    
